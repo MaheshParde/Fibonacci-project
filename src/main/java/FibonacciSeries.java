a5b6c19ba09f0c5eacb075584dc582f1c196768b
LAPTOP-58L05II1\parde <pardemahesh9595@gmai.com>
2021-02-01 20:36:38 +0530

    first commit

---
+++ src/main/java/FibonacciSeries.java
@@ -0,0 +1,17 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+
+        int count = 35, num1 = 0, num2 = 1;
+        System.out.print("Fibonacci Series of "+count+" numbers:");
+
+        int i=1;
+        while(i<=count)
+        {
+            System.out.print(num1+" ");
+            int sumOfPrevTwo = num1 + num2;
+            num1 = num2;
+            num2 = sumOfPrevTwo;
+            i++;
+        }
+    }
+}
